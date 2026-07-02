ee6fa2dbad5fb54b50e4572c12a4e14ae459dff8
SUMIT CHAUDHARI <sumit@SUMITs-MacBook-Air-2.local>
2026-07-02 18:13:33 +0530

    Day 2: Time Complexity (Big O) completed

---
+++ Day2(Big O).py
@@ -0,0 +1,23 @@
+# 1) complexity batao.   x = 100
+#                        print(x)
+# --> O(1) constant time complexity.
+
+# 2) complexity batao.   
+# for i in range(n):
+#    print(i)
+# --> O(n) linear time complexity.
+
+# 3) complexity batao.
+#for i in range(n):
+#    for j in range(n):
+#        print(i,j)
+#--> O(n^2) quadratic time complexity.
+
+# 4) printing string for 3 times 
+name = input("Enter your name : ")
+for i in range(3):
+    print("name : ", name)
+
+# 5) printing numbers from 1 to 10
+for i in range (0, 10):
+    print(i+1)
