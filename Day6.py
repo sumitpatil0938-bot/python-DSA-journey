becfeeb08de0a671efcf4f8d75fe28a6580836d7
SUMIT CHAUDHARI <sumit@SUMITs-MacBook-Air-2.local>
2026-07-06 18:03:43 +0530

    Day 6: Linear Search in Arrays

---
+++ Day6.py
@@ -0,0 +1,65 @@
+# LINEAR SEARCH 
+
+# 1) arr = [10,20,30,40,50] target = 30
+arr = [10,20,30,40,50]
+target = 30
+for num in arr:
+    num = target
+    print(target)
+    break
+
+# 2) arr = [10,20,30,40,50] target = 40 print index 
+arr = [10,20,30,40,50]
+target = 40 
+for i in range(len(arr)):
+    if arr[i] == target:
+     print(i) 
+     break
+
+# 3) arr = [5,10,15,20] target = 100 , expect not found 
+arr = [5,10,15,20]
+target = 100
+found = False
+for num in arr:
+   if num == target:
+      found = True
+      
+      
+if found:
+   print("Found")
+else : 
+    print("Not Found")
+
+
+
+# 4) arr = [7,14,21,28,35] target = 21 , print found at index 2 
+arr = [7,14,21,28,35]
+target = 21
+for i in range(len(arr)):
+   if arr[i] == target:
+      print(i)
+      break
+
+# 5) arr = [1,2,3,4,5] , target 5 , print found 
+arr = [1,2,3,4,5]
+target = 5
+found = False
+for num in arr:
+   if num == target:
+      found = True 
+
+if (found):
+   print ("Found")
+else:
+   print("Not found" )
+
+
+# ye jo main daily questions solve karta hu wo main chat gpt se puch tha hu questions de ne ke liye aur uski baare maine theri bahi padta hu aur uski conceps samjta hu
+# baad main daily 5 questions solve karta hu useese related okay 
+# chatgpt questions deta hai aur main code lekta hu 
+# error aane ke badd chat gpt puch tha hu aisa kyu hu aur fir wo muje batata hain naki code deta haina aur fir main use solve karta hau  
+
+
+# MAIN YE DSA TAB TAK SOLVE KARUNG JAB TAK MAIN APPLE MAI JOD KE LIYE READY NAHI HOJATA YA APPLE MAIN JOIN NAHI HOJA UNGA 
+# APPLE MAIN HEAD QUARTER SILICON VALLY ........... THAT'S MY COMMITMENT APPLE BE READY I'AM COMING TO HAND OVER YOUR COMPANY .....
+
