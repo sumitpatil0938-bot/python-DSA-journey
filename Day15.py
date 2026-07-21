99e3f6d8abc02177e1d9d8cd4ed998b6eafd34a7
SUMIT CHAUDHARI <sumit@SUMITs-MacBook-Air-2.local>
2026-07-21 20:45:18 +0530

    Day 15: Sorting Introduction completed

---
+++ Day15.py
@@ -0,0 +1,39 @@
+# SORTING INTRODUCTION
+
+# --> matlab data ko kisi pattern main arrange karna.
+
+# TIME COMPLEXITY
+# Bubble Sort     -> O(n²)
+# Selection Sort  -> O(n²)
+# Insertion Sort  -> O(n²)
+
+# 1) arr = [50,10,40,20,30] Ascending order likho manually.
+# => arr = [10,20,30,40,50]
+
+# 2) arr = [5,1] Swap karne ke baad output?
+# => arr = [1,5]
+
+# 3) Ascending aur Descending order ka difference likho. Example ke saath.
+# => Ascinding --> matlab arrangement of numbers from small to big. 
+#     example -> array = [1,2,3,4,5,6,7,8,9,10]
+#    Descending = matlab arrangement of numbers from big to samll
+#.    example -> array = [10,9,8,7,6,5,4,3,2,1]
+
+# 4) Real life ke 5 examples likho jahan sorting use hoti hai.
+# => a) studrnt ki rank decide karne ke liye.
+#    b) online shopping website.
+#    c) contact list in mobile A-Z.
+#    d) Library books management.
+#    e) Employee salary record.
+
+# 5) Agar Binary Search use karni ho to pehle kya karna padega?
+# => bahi pehle to unsorted ko sorted foem mai arrange karna padega 
+#    sorting algoritm ke help se aur fir ham uspe binary serach ka use kar sakte hai.
+#    socho:
+#          taki hame hamara target jaldi mil sake aur 
+#          time bach jaye 
+# example:
+# samjo mobile contact ke hisab se 
+# first --> numbers in unsorted form
+# second --> bubble sorting use kiya --> ab sorted form main hai
+# third --> ab soch mujhe number dhundna hai sumit ka to main binary search use karunga
