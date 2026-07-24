48ae1dee36b045219c1502277472a10bb40fc8ff
SUMIT CHAUDHARI <sumit@SUMITs-MacBook-Air-2.local>
2026-07-24 23:30:28 +0530

    Day 18: Learned Selection Sort with dry runs, pass-wise logic, comparisons, swaps, and practice problems

---
+++ Day18.py
@@ -0,0 +1,25 @@
+# SELECTION SORT
+
+# 1) arr = [40,20,10] Pass 1 ke baad array ?
+# --> arr = [10,20,40]
+
+# 2) arr = [30,10,20] Pass 2 ke baad array ?
+# --> arr = [10,30,20] pass 1 
+#     arr = [10,20,30] pass 2
+
+# 3) 5 elements hain. Kitne passes?
+# --> n - 1 = 4 passes 
+
+# 4) Selection Sort mein har pass mein kya find karte hain ?
+# --> minimum find karte hain.
+
+# 5) Bubble Sort aur Selection Sort mein main difference ?
+# --> Bubble sort = 1 pass main swap hota hai jab tak largest elemant of array last tak na puach jaye. 
+#     Selection sort = 1 pass main ek hi baar swap karta hai.
+
+# MINI CHALLENGE (WITHOUT CODE)
+# arr = [25,15,35,10]
+# pass 1 => arr = [10,15,35,25]
+# pass 2 => arr = [10,15,35,25] no swap needed , already on correct position
+# pass 3 => arr = [10,15,25,35]
+# final array ===> [10,15,25,35]
