baba21af444b454059027f41e0280eddb46bb841
SUMIT CHAUDHARI <sumit@SUMITs-MacBook-Air-2.local>
2026-07-25 22:09:36 +0530

    Day 19: Learned Insertion Sort with dry runs and mini challenge

---
+++ Day19.py
@@ -0,0 +1,28 @@
+# INSRETION SORT
+
+# 1) pass 1 ke baad array ? arr = [7,4]
+# --> arr = [7,4]
+#     arr = [4,7]
+
+# 2) pass 1 ke baad ? arr = [8,5,2]
+# --> arr = [8,5,2]
+#     arr = [5,8,2]
+
+# 3) final sorted array ? arr = [8,5,2]
+# --> arr = [8,5,2]
+#     arr = [5,8,2]
+#     arr = [2,5,8]
+#     arr = [2,5,8] Final sorted array 
+
+# 4) Insertion sort main key kya hota hai ?
+# --> key = arr[i]
+
+# 5) Insertion sort swap karta hai ya shift karta hai ? 
+# --> shift karta hai
+
+# MINI CHALLENGE 
+# Q) Without code solve manually arr = [9,4,7,2]
+# pass 1 => arr = [4,9,7,2]
+# pass 2 => arr = [4,7,9,2]
+# pass 3 => arr = [2,4,7,9]
+# pass 3 => arr = [2,4,7,9] Final sorted array 
