6b6be987ec11b0e35464a19b06125c89cd0eed6f
SUMIT CHAUDHARI <sumit@SUMITs-MacBook-Air-2.local>
2026-07-28 17:35:33 +0530

    DSA Day 22: String Basics completed

---
+++ Day22.py
@@ -0,0 +1,33 @@
+# STARING BASICS
+
+# 1) word = "CYBER" index of b ?
+# --> index of B is 2
+
+# 2) word = "PYTHON" Last character ?
+# --> N print(word[-1])
+
+# 3) word = "APPLE" Length ?
+# --> print(len(word)) = 5
+
+# 4) word = "HELLO" word[-2] =? ?
+# --> L (negative indexing)
+
+# 5) word = "DOG" Characters one by one ?
+# --> for i in range(len(word)):
+#           print(word[i])
+#    D
+#    O
+#    G
+
+
+# NOTES : Sstring Basics
+
+# -> String = Collection of characters.
+# -> Index starts from 0
+# -> negative indexing starts from -1
+# -> Last charater = str[-1]
+# -> Length = len(str)
+# -> Traversal = for loop 
+# -> String is immutable(no change).
+# -> index access = O(1)
+# -> Traversal = O(n)
