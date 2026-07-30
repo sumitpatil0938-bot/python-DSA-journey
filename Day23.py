d75d3d90341f2c307baa477af0cae642ff76ad60
SUMIT CHAUDHARI <sumit@SUMITs-MacBook-Air-2.local>
2026-07-30 18:28:45 +0530

    DSA Day 23: String Operations completed

---
+++ Day23.py
@@ -0,0 +1,52 @@
+# STRING OPERATION
+
+# 1) star = "APPLE" character at index 2
+# --> star = "apple"
+#     print(star[2])
+
+# 2) str = "Cyber" Is "C" present ?
+# --> str = "Cyber"
+#     print("C" in str)
+
+# 3) "abc" == "abc"
+# --> Output = True
+
+# 4) "abc" == "ABC"
+# --> Output = False
+
+# 5) "hi"*5
+# --> Output = hihihihihi
+
+
+# MINI CHALLENGE WITHOUT CODE
+
+# 1) str = "sumit" length ?
+# --> str = "sumit"
+#     print(len(str))
+
+# 2) str = "apple" last character ?
+# --> str = "apple"
+#     print(str[-1])
+
+# 3) str = "python" Index 3 ?
+# --> str = "python"
+#     print(str[3])
+
+# 4) "Dog" == "dog" ?
+# --> Output = False
+
+# 5) "sumit"*3
+# --> sumitsumitsumit
+
+
+# SHORT REVISION NOTES
+
+# String = sequence of character 
+# Traversal = Visit every character
+# Search = in
+# Join = +
+# Repeat = *
+# Compare = "=="
+# Indexing starts from 0
+# Negative indexing starts from -1
+# Strings are immutable ( no change)
