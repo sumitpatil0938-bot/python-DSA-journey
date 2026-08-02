7f434c6985bd42e58420f2a712dc4740cffd8d09
SUMIT CHAUDHARI <sumit@SUMITs-MacBook-Air-2.local>
2026-08-02 19:44:00 +0530

    DSA Day 24: String Slicing & Methods completed

---
+++ Day24.py
@@ -0,0 +1,39 @@
+# STRING SLICING & STRING METHODS
+
+# 1) word = "COMPUTER"
+# --> print(word[0:3])  # Output: COM
+
+# 2) word = "PYTHON"
+# --> print(word[2:])  # Output: THON
+
+# 3) word = "PROGRAMMING"
+# --> print(word[::-1])  # Output: GNIMMARGORP
+
+# 4) word = "HELLO" print uppercase ?
+# --> print(word.upper())  # Output: HELLO
+
+# 5) word = "world" print lowercase ? 
+# --> print(word.lower())  # Output: world
+
+
+# MINI CHALLENGE WITHOUT CODE
+
+# 1) word = "PYTHON" slice from index 1 to 4
+# --> word = "PYTHON"
+#     print(word[1:5])  # Output: YTHO
+
+# 2) word = "PROGRAM" slice from index 3 to end
+# --> word = "PROGRAM"
+#     print(word[3:])  # Output: GRAM
+
+# 3) word = "HELLO" find word O ?
+# --> word = "HELLO"
+#     print(word.find("O"))  # Output: 4
+
+# 4) word = "WORLD" replace W with M ?
+# --> word = "WORLD"
+#     print(word.replace("W", "M"))  # Output: MORLD
+
+# 5) text "I LOVE PYTHON" split into words ?
+# --> text = "I LOVE PYTHON"
+#     print(text.split())  # Output: ['I', 'LOVE', 'PYTHON']
