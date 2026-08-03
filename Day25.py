bdb09271e2ad2f0568adefb2897d6d508789ba95
SUMIT CHAUDHARI <sumit@SUMITs-MacBook-Air-2.local>
2026-08-03 18:34:06 +0530

    DSA Day 25: Palindrome String completed

---
+++ Day25.py
@@ -0,0 +1,47 @@
+# PALINDROME STRING
+
+# 1) word = "RADAR" is palindrome
+# --> YES
+# Forward -> RADAR
+# Backword -> R A D A R
+# if word are same from forward and backword then it is palindrome
+
+# 2) word = "APPLE" is palindrome
+# --> NO Palindrome
+
+# 3) word = "LEVEL" is palindrome
+# --> YES
+
+# 4) word = "PYTHON" is palindrome
+# --> NO Palindrome
+
+# 5) word = "PYTHON" is palindrome
+# --> NO Palindrome
+
+# 6) Palindrome check karne ke easist way ?
+# --> word == word[::-1]
+
+# 7) can we solve without slicing ?
+# --> YES 
+# Loop se reverse bana sakte hai.
+
+# 8) time complexity ?
+# --> O(n)
+
+
+# REVISION NOTES
+
+# Forward == Backword
+
+# Reverse
+# Word[::-1]
+
+# check
+
+# if word == word[::-1]
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+# time = O(n)
+
+# Space = O(1) using sliciing comparision
