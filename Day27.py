a081e37283c0cf9ed2b72ea52162d32d73fb0e2c
SUMIT CHAUDHARI <sumit@SUMITs-MacBook-Air-2.local>
2026-08-08 23:07:01 +0530

    DSA Day 27: Anagram concepts and frequency analysis

---
+++ Day27.py
@@ -0,0 +1,44 @@
+# ANAGRAMS
+
+# 1) "listen"
+#    "silent"
+# --> YES
+
+# 2) "hello"
+#    "world"
+# --> NO
+
+# 3) "race"
+#    "care"
+# --> YES
+
+# 4) "apple"
+#    "papel"
+# --> YES
+
+# 5) "rat"
+#    "car"
+# --> NO
+
+
+# MINI CHALLENGE
+
+# 1) str1 = "evil"
+#    str2 = "vile"
+# --> YES
+
+# 2) str1 = "aabb"
+#    str2 = "abab"
+# --> YES
+
+# 3) str1 = "aabb"
+#    str2 = "abbb"
+# --> NO
+
+# 4) str1 = "triangle"
+#    str2 = "integral"
+# --> YES
+
+# 5) str1 = "abc"
+#    str2 = "abcd"
+# Sorting karne se pehle kaunsa check karna smart hoga 
